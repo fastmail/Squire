15c45cc677c338989b430866055344efb51ae908
Matthew Borden <borden0108@gmail.com>
2014-07-08 16:00:44 +1000

    Working: On Functionality

--- ui/Squire-UI.js
+++ ui/Squire-UI.js
@@ -22,18 +22,23 @@ $(document).ready(function () {
                 "No element was defined for the editor to inject to."
             );
         }
-
         var iframe = document.createElement('iframe');
         var div = document.createElement('div');
-
+        div.className = 'Squire-UI';
+        
         $(div).load(buildPath + 'Squire-UI.html', function () {
             $('.item').click(function () {
-                console.log($(this).parent().parent().parent());
+                var me = $(this);
+                var iFrame = me.parents('.Squire-UI').next('iframe').first()[0];
+                var editor = iFrame.contentWindow.document;
+                var action = me.data('action');
+                console.log(editor, action);
             });
         });
 
         $(container).append(div);
         $(container).append(iframe);
+
         editor = new Squire(iframe.contentWindow.document);
         return editor;
     };
