3c6ea38ca6f64b5484ebf33da329e2bede3b3a33
Gert Sønderby <gert@one.com>
2015-06-16 12:39:31 +0200

    Tests run in latest Chrome, FF, Safari + IE10 and 11.

--- test/squire.spec.js
+++ test/squire.spec.js
@@ -12,7 +12,9 @@ expect = expect.clone()
     })
     .addAssertion('[not] to contain HTML', function (expect, editor, expectedValue) {
         this.errorMode = 'bubble';
-        expect(editor.getHTML(), '[not] to be', expectedValue);
+        var actualHTML = editor.getHTML().replace(/<br>/g, '');
+        // BR tags are inconsistent across browsers. Removing them allows cross-browser testing.
+        expect(actualHTML, '[not] to be', expectedValue);
     });
 
 describe('Squire RTE', function () {
@@ -25,45 +27,46 @@ describe('Squire RTE', function () {
 
     function selectAll(editor) {
         var range = doc.createRange();
-        range.setStartBefore(doc.body.childNodes.item(0));
-        range.setEndAfter(doc.body.childNodes.item(0));
+        range.setStart(doc.body.childNodes.item(0), 0);
+        range.setEnd(doc.body.childNodes.item(0), doc.body.childNodes.item(0).childNodes.length);
         editor.setSelection(range);
     }
 
-    describe('removeAllFormatting()', function () {
+    describe('removeAllFormatting', function () {
         // Trivial cases
         it('removes inline styles', function () {
-            var startHTML = '<div><i>one</i> <b>two</b> <u>three</u> <sub>four</sub> <sup>five</sup><br></div>';
+            var startHTML = '<div><i>one</i> <b>two</b> <u>three</u> <sub>four</sub> <sup>five</sup></div>';
             editor.setHTML(startHTML);
             expect(editor, 'to contain HTML', startHTML);
             selectAll(editor);
             editor.removeAllFormatting();
-            expect(editor, 'to contain HTML', '<div><br>one two three four five</div>');
+            expect(editor, 'to contain HTML', '<div>one two three four five</div>');
         });
         it('removes block styles', function () {
-            var startHTML = '<div><blockquote>one<br></blockquote><ul><li>two<br></li></ul>' + 
-                '<ol><li>three<br></li></ol><table><tbody><tr><th>four<br></th><td>five<br></td></tr></tbody></table></div>';
+            var startHTML = '<div><blockquote>one</blockquote><ul><li>two</li></ul>' + 
+                '<ol><li>three</li></ol><table><tbody><tr><th>four</th><td>five</td></tr></tbody></table></div>';
             editor.setHTML(startHTML);
             expect(editor, 'to contain HTML', startHTML);
             selectAll(editor);
             editor.removeAllFormatting();
-            expect(editor, 'to contain HTML', '<div><br>one</div><div><br>two</div><div><br>three</div><div><br>four</div><div><br>five</div>');
+            var expectedHTML = '<div><div>one</div><div>two</div><div>three</div><div>four</div><div>five</div></div>';
+            expect(editor, 'to contain HTML', expectedHTML);
         });
 
         // Potential bugs
         it('removes styles that begin inside the range', function () {
-            var startHTML = '<div>one <i>two three four five</i><br></div>';
+            var startHTML = '<div>one <i>two three four five</i></div>';
             editor.setHTML(startHTML);
             expect(editor, 'to contain HTML', startHTML);
             var range = doc.createRange();
             range.setStart(doc.body.childNodes.item(0), 0);
             range.setEnd(doc.getElementsByTagName('i').item(0).childNodes.item(0), 4);
             editor.removeAllFormatting(range);
-            expect(editor, 'to contain HTML', '<div>one two <i>three four five</i><br></div>');
+            expect(editor, 'to contain HTML', '<div>one two <i>three four five</i></div>');
         });
 
         it('removes styles that end inside the range', function () {
-            var startHTML = '<div><i>one two three four</i> five<br></div>';
+            var startHTML = '<div><i>one two three four</i> five</div>';
             editor.setHTML(startHTML);
             expect(editor, 'to contain HTML', startHTML);
             var range = doc.createRange();
@@ -74,7 +77,7 @@ describe('Squire RTE', function () {
         });
 
         it('removes styles enclosed by the range', function () {
-            var startHTML = '<div>one <i>two three four</i> five<br></div>';
+            var startHTML = '<div>one <i>two three four</i> five</div>';
             editor.setHTML(startHTML);
             expect(editor, 'to contain HTML', startHTML);
             var range = doc.createRange();
@@ -85,34 +88,34 @@ describe('Squire RTE', function () {
         });
 
         it('removes styles enclosing the range', function () {
-            var startHTML = '<div><i>one two three four five</i><br></div>';
+            var startHTML = '<div><i>one two three four five</i></div>';
             editor.setHTML(startHTML);
             expect(editor, 'to contain HTML', startHTML);
             var range = doc.createRange();
             range.setStart(doc.getElementsByTagName('i').item(0).childNodes.item(0), 4);
             range.setEnd(doc.getElementsByTagName('i').item(0).childNodes.item(0), 18);
             editor.removeAllFormatting(range);
-            expect(editor, 'to contain HTML', '<div><i>one </i>two three four<i> five</i><br></div>');
+            expect(editor, 'to contain HTML', '<div><i>one </i>two three four<i> five</i></div>');
         });
 
         it('removes nested styles and closes tags correctly', function () {
-            var startHTML = '<table><tbody><tr><td>one<br></td></tr><tr><td>two<br></td><td>three<br></td></tr><tr><td>four<br></td><td>five<br></td></tr></tbody></table>';
+            var startHTML = '<table><tbody><tr><td>one</td></tr><tr><td>two</td><td>three</td></tr><tr><td>four</td><td>five</td></tr></tbody></table>';
             editor.setHTML(startHTML);
             expect(editor, 'to contain HTML', startHTML);
             var range = doc.createRange();
             range.setStart(doc.getElementsByTagName('td').item(1), 0);
             range.setEnd(doc.getElementsByTagName('td').item(2), doc.getElementsByTagName('td').item(2).childNodes.length);
             editor.removeAllFormatting(range);
-            expect(editor, 'to contain HTML', '<table><tbody><tr><td>one<br></td></tr></tbody></table>' +
-                '<div><br>two</div>' +
-                '<div><br>three</div>' +
-                '<table><tbody><tr><td>four<br></td><td>five<br></td></tr></tbody></table>');
+            expect(editor, 'to contain HTML', '<table><tbody><tr><td>one</td></tr></tbody></table>' +
+                '<div>two</div>' +
+                '<div>three</div>' +
+                '<table><tbody><tr><td>four</td><td>five</td></tr></tbody></table>');
         });
     });
 
     afterEach(function () {
         editor = null;
         var iframe = document.getElementById('testFrame');
-        iframe.src = 'about:blank';
+        iframe.src = 'blank.html';
     });
 });
